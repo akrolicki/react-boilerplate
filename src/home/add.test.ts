b41649e5697ce23934f0954cadf6cb11c479ac8b
akrolicki <adam.krolicki@widelab.co>
2023-02-02 15:49:10 +0100

    add basic jest configuration + example test

---
+++ src/home/add.test.ts
@@ -0,0 +1,10 @@
+import { add } from './add';
+
+describe('add', () => {
+  it('should correctly add two numbers', () => {
+    expect(add(2, 3)).toEqual(5);
+  });
+  it('should correctly add two numbers even if number is negative', () => {
+    expect(add(2, -3)).toEqual(-1);
+  });
+});
