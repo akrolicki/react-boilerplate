8fdf6b92c2760bc17cf0e5d6465d58c0d1f88382
akrolicki <adam.krolicki@widelab.co>
2023-02-02 15:59:00 +0100

    just test

--- src/home/add.test.ts
+++ src/home/add.test.ts
@@ -5,6 +5,6 @@ describe('add', () => {
     expect(add(2, 3)).toEqual(5);
   });
   it('should correctly add two numbers even if number is negative', () => {
-    expect(add(2, -3)).toEqual(111);
+    expect(add(2, -3)).toEqual(1111);
   });
 });
