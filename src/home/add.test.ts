3f9f4dd533ecbcb2cf111e880a8f4ca0f6dee8ad
akrolicki <adam.krolicki@widelab.co>
2023-02-02 20:06:17 +0100

    test prettier on git commit

--- src/home/add.test.ts
+++ src/home/add.test.ts
@@ -4,8 +4,7 @@ describe('add', () => {
   it('should correctly add two numbers', () => {
     expect(add(2, 3)).toEqual(5);
   });
-  it('should correctly add two numbers even if number is negative', 
-() => {
- expect(add(2, -3)).toEqual(-1);
+  it('should correctly add two numbers even if number is negative', () => {
+    expect(add(2, -3)).toEqual(-1);
   });
 });
